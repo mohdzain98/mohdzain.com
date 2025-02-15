731dfb948cd6061d54ec9bb36d387229b8170933
Mohd Zain <zainmohd1998@gmail.com>
2025-02-15 20:20:06 +0530

    removed blinking cursor

--- src/Components/Home/TypingEffect.js
+++ src/Components/Home/TypingEffect.js
@@ -1,26 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import '../Styling/Intro.css'
+import React, { useState, useEffect } from "react";
+import "../Styling/Intro.css";
 
-const TypingEffect = ({ text, speed }) => {
-    const [displayedText, setDisplayedText] = useState('');
-    const [index, setIndex] = useState(0); 
+const TypingEffect = ({ className, text, speed }) => {
+  const [displayedText, setDisplayedText] = useState("");
+  const [index, setIndex] = useState(0);
 
-    useEffect(() => {
-        if (index < text.length) {
-            const timeout = setTimeout(() => {
-                setDisplayedText(prev => prev + text.charAt(index)); 
-                setIndex(index + 1); 
-            }, speed);
-            return () => clearTimeout(timeout);
-        }
-    }, [index, text, speed]);
+  useEffect(() => {
+    if (index < text.length) {
+      const timeout = setTimeout(() => {
+        setDisplayedText((prev) => prev + text.charAt(index));
+        setIndex(index + 1);
+      }, speed);
+      return () => clearTimeout(timeout);
+    }
+  }, [index, text, speed]);
 
-    return (
-        <div id='typing'>
-            <span>{displayedText}</span>
-            <span className="typing-cursor"></span> {/* Blinking cursor */}
-        </div>
-    );
+  return (
+    <div id="typing">
+      <h2 className={className}>{displayedText}</h2>
+      {/* <span className="typing-cursor"></span> Blinking cursor */}
+    </div>
+  );
 };
 
 export default TypingEffect;
