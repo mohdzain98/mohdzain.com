ad2dc6ea7956b99b561a91cafa0ceaca3f9e99e8
Mohd Zain <zainmohd1998@gmail.com>
2025-02-15 20:18:03 +0530

    add siemap

---
+++ sitemap.js
@@ -0,0 +1,30 @@
+const { SitemapStream, streamToPromise } = require("sitemap");
+const fs = require("fs");
+const path = require("path");
+
+routes = [
+  { url: "/", changefreq: "daily", priority: 1.0 },
+  { url: "/projects", changefreq: "monthly", priority: 0.8 },
+  { url: "/blogs", changefreq: "monthly", priority: 0.8 },
+  {
+    url: "/blogs/color-images-cryptosystem",
+    changefreq: "monthly",
+    priority: 0.8,
+  },
+  { url: "/gitrepos", changefreq: "monthly", priority: 0.8 },
+];
+// Generate the sitemap
+const sitemap = new SitemapStream({ hostname: "https://mohdzain.com" });
+
+routes.forEach((route) => {
+  sitemap.write(route);
+});
+
+sitemap.end();
+
+streamToPromise(sitemap)
+  .then((data) => {
+    fs.writeFileSync(path.join(__dirname, "public", "sitemap.xml"), data);
+    console.log("✅ Sitemap generated successfully!");
+  })
+  .catch((err) => console.error("Error generating sitemap:", err));
