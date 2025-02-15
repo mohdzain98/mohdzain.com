277111fadf21f96e720df728b5d5060e443b22e4
Mohd Zain <zainmohd1998@gmail.com>
2025-02-15 20:22:44 +0530

    updated UI of intro section

--- src/Components/Home/Intro.js
+++ src/Components/Home/Intro.js
@@ -1,42 +1,121 @@
-import React from 'react'
-import '../Styling/Intro.css'
-import {Link} from 'react-router-dom'
-import 'animate.css'
-import { AnimatedOnScroll } from "react-animated-css-onscroll";
-import { useMediaQuery } from 'react-responsive'
-import TypingEffect from './TypingEffect';
+import React from "react";
+import "../Styling/Intro.css";
+import "animate.css";
+import { useMediaQuery } from "react-responsive";
+import Ribbon from "../Ribbon";
 
 const Intro = () => {
-  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
 
   return (
-    <section className='home'>
-        <div className='image'>
-            <img src={require('../../Assets/zain.JPG')}  alt="altpdf"  width={330} height={330} style={{borderRadius:'50%'}}/>
-        </div>
-        <div className='content' id='con'>
-          <AnimatedOnScroll animationIn="animate__animated animate__fadeInDown">
-            <h2 className='name' style={isTabletOrMobile?{fontSize:'46px'}:{}}><span style={{fontSize:"30px"}}>Hi! I'm</span> Mohd Zain</h2>
-          </AnimatedOnScroll>
-          <AnimatedOnScroll animationIn="animate__animated animate__fadeInDown">
-            <h5 className='title'><span>Currently,</span> <TypingEffect text='Data Science Intern at Sigmoid Analytics' speed={50}/></h5>
-          </AnimatedOnScroll>
-          <AnimatedOnScroll animationIn="animate__animated animate__fadeInLeft">
-            <p style={{textAlign:'justify'}} className='mt-4'>I hold an M.Tech in Data Analytics from, <Link to={'https://iitism.ac.in'} style={{textDecoration:'none',color:'red'}} target='_blank' rel="noopener">IIT (ISM) Dhanbad</Link>, with a deep interest in Machine Learning, Deep Learning, and ML-based web application development. My experience spans multiple projects, including the development of Docschat, an LLM-based web app. Currently, I am working as a Data Science Intern at <Link to={'https://sigmoid.com'} target='_blank' rel="noopener" style={{textDecoration:'none',color:'red'}}>Sigmoid</Link>, applying my skills to real-world challenges.</p>
-          </AnimatedOnScroll>
-          <AnimatedOnScroll animationIn="animate__animated animate__fadeInRight">
-            <p style={{textAlign:'justify'}}>
-                <span style={{fontSize:'20px',color:'#2a201c'}}>Thesis: </span>In my M. Tech <Link to='/blogs/color-images-cryptosystem'>Thesis</Link> at IIT Dhanbad, I focused on developing a DNA-based color image cryptosystem designed to protect image transmission from network adversaries. The cryptosystem employed a combination of binary functions, applicable to both traditional and DNA-based techniques, to ensure security for colored natural images.
+    <section className="home py-5" id="about">
+      <center>
+        <Ribbon value={"About Me"} color={"blue"} />
+      </center>
+      <div className={`pb-3 row`}>
+        <div className="col-md-5 d-flex flex-column">
+          <center>
+            <div className="image flex-grow-1">
+              <img
+                src={require("../../Assets/zain.JPG")}
+                alt="mohdzain"
+                width={isTabletOrMobile ? 230 : 330}
+                height={isTabletOrMobile ? 230 : 330}
+                style={{ borderRadius: "50%" }}
+                className="align-self-center"
+              />
+            </div>
+          </center>
+          <div className="text-secondary mt-auto">
+            <h3 className="h4 mb-4 mt-2 fw-bold">About</h3>
+            <p className="mt-4">
+              📚 M.Tech in Data Analytics from IIT (ISM) Dhanbad
+              <br />
+              🎓 B.Tech in Computer Science and Engineering from UTU.
+              <br />
+              💻 Passionate about applied Machine Learning, Gen AI.
             </p>
-          </AnimatedOnScroll>
-          <AnimatedOnScroll animationIn="animate__animated animate__fadeInUp">
-            <p>
-            <span style={{fontSize:'20px',color:'#2a201c'}}>Contact: </span> zainmohd1998 [at] gmail [dot] com
-            </p>
-          </AnimatedOnScroll> 
+          </div>
         </div>
+        <div className="col-md-7 mx-auto">
+          <div className="row align-items-center">
+            <div className="mx-auto">
+              <div className="text-secondary">
+                <h3 className="h4 mb-4 fw-bold">Work Experience</h3>
+                <div className="ps-1">
+                  <p className="fw-bold" style={{ marginBottom: "1px" }}>
+                    Sigmoid | Data Science Trainee{" "}
+                    <span
+                      className={`float-end ${
+                        isTabletOrMobile ? "invisible" : ""
+                      }`}
+                      style={{ fontSize: "14px" }}
+                    >
+                      Jan 2025 - present
+                    </span>
+                    {isTabletOrMobile && (
+                      <p style={{ fontSize: "14px" }}>Jan 2025 - present</p>
+                    )}
+                  </p>
+                  <p style={{ textAlign: "justify" }}>
+                    {" "}
+                    I am working on revenue growth management econometrics,
+                    leveraging advanced statistical modeling and machine
+                    learning techniques to analyze pricing, demand elasticity,
+                    and market dynamics. My work involves developing econometric
+                    models, including LOESS, GAM, and mixed models, to drive
+                    data-driven decision-making for revenue and profit
+                    maximization.
+                  </p>
+                </div>
+                <div className="ps-1">
+                  <p className="fw-bold" style={{ marginBottom: "1px" }}>
+                    Sigmoid | Data Science Intern{" "}
+                    <span
+                      className={`float-end ${
+                        isTabletOrMobile ? "invisible" : ""
+                      }`}
+                      style={{ fontSize: "14px" }}
+                    >
+                      Aug 2024 - Dec 2024
+                    </span>
+                    {isTabletOrMobile && (
+                      <p style={{ fontSize: "14px" }}>Aug 2024 - Dec 2024</p>
+                    )}
+                  </p>
+                  <p>
+                    <ul>
+                      <li>
+                        Got selected in RGMx project in econometrics team,
+                        building predictive models and validating their
+                        performance.
+                      </li>
+                      <li>
+                        Enhanced SQL and Python proficiency through hands-on
+                        tasks, including database querying and data manip-
+                        ulation.
+                      </li>
+                      <li>
+                        Participated in training programs on Python, SQL, and
+                        visualization techniques to strengthen foundational
+                        skills.
+                      </li>
+                    </ul>
+                  </p>
+                </div>
+                <h3 className="h4 mt-5 mb-3 fw-bold">Academic Research</h3>
+                <p style={{ textAlign: "justify" }}>
+                  Developed a DNA-based color image cryptosystem during my
+                  M.Tech thesis, focusing on secure image transmission through
+                  innovative binary functions and DNA-based encoding techniques.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Intro
+export default Intro;
