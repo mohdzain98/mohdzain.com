e62136b79ec4443e0d707ee8b17ebc8307cf7eee
Mohd Zain <zainmohd1998@gmail.com>
2025-02-15 20:20:54 +0530

    add new UI component

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,27 +1,28 @@
-import React,{useEffect} from 'react'
-import Intro from './Home/Intro'
-import Careerpath from './Home/Careerpath'
-import Skills from './Home/Skills'
-import Projects from './Home/Projects'
-import Social from './Home/Social'
-import Footer from './Home/Footer'
+import React, { useEffect } from "react";
+import Intro from "./Home/Intro";
+import Careerpath from "./Home/Careerpath";
+import Skills from "./Home/Skills";
+import Projects from "./Home/Projects";
+import Social from "./Home/Social";
+import Footer from "./Home/Footer";
+import Head from "./Home/Head";
 
 const Home = (props) => {
   useEffect(() => {
-    document.title = "Mohd Zain"; 
-  }, [])
-
+    document.title = "Mohd Zain";
+  }, []);
 
   return (
     <>
-    <Intro setProgress={props.setProgress} />
-    <Careerpath setProgress={props.setProgress}/>
-    <Skills setProgress={props.setProgress}/>
-    <Projects setProgress={props.setProgress}/>
-    <Social setProgress={props.setProgress}/>
-    <Footer setProgress={props.setProgress}/>
+      <Head setProgress={props.setProgress} />
+      <Intro setProgress={props.setProgress} />
+      <Careerpath setProgress={props.setProgress} />
+      <Skills setProgress={props.setProgress} />
+      <Projects setProgress={props.setProgress} />
+      <Social setProgress={props.setProgress} />
+      <Footer setProgress={props.setProgress} />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
