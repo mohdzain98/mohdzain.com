0c40273d7e8e4e5919bb521de14fe093a72901aa
Mohd Zain <zainmohd1998@gmail.com>
2025-10-12 23:39:39 +0530

    chore : code refactor

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import{BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Topbar from './Components/Bars/Topbar';
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
@@ -12,18 +12,18 @@ import Gitrepos from './Components/Gitrepos';
 function App() {
   return (
     <>
-    <Router>
-      <ScrollToTop/>
-      <Topbar/>
-      <Navbar/>
-      <Routes>
-        <Route exact path="/" element={<Home />}></Route>
-        <Route exact path='/projects' element={<Allprojects/>}></Route>
-        <Route exact path='/blogs' element={<Blogs/>}></Route>
-        <Route exact path='/blogs/color-images-cryptosystem' element={<CimagesCrypt/>}></Route>
-        <Route exact path='/gitrepos' element={<Gitrepos/>}></Route>
-      </Routes>
-    </Router>
+      <Router>
+        <ScrollToTop />
+        <Topbar />
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path='/projects' element={<Allprojects />}></Route>
+          <Route exact path='/blogs' element={<Blogs />}></Route>
+          <Route exact path='/blogs/color-images-cryptosystem' element={<CimagesCrypt />}></Route>
+          <Route exact path='/gitrepos' element={<Gitrepos />}></Route>
+        </Routes>
+      </Router>
     </>
   );
 }
