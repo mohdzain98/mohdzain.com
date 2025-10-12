176137bc1d9860abce9d7e3feda678136a1e5779
Mohd Zain <zainmohd1998@gmail.com>
2025-10-12 23:40:17 +0530

    feat : added research component

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,23 +1,39 @@
 import React, { useEffect } from "react";
 import Intro from "./Home/Intro";
 import Careerpath from "./Home/Careerpath";
-import Skills from "./Home/Skills";
+// import Skills from "./Home/Skills";
 import Projects from "./Home/Projects";
 import Social from "./Home/Social";
 import Footer from "./Home/Footer";
 import Head from "./Home/Head";
+import Research from "./Home/Research";
+import Skills2 from "./Home/Skills2";
 
 const Home = (props) => {
   useEffect(() => {
     document.title = "Mohd Zain";
   }, []);
 
+  const paper = {
+    title:
+      "Design of DNA-Based Color Image Cryptosystem and Its Security Analysis",
+    authors: "Mohd Zain, Coauthor Name",
+    abstract:
+      "This paper presents a DNA-based encryption technique for color images using binary functions and DNA encoding...",
+    publishedIn: "Journal of Cryptography, Vol. X",
+    publishedDate: "June 2024",
+    doi: "10.1234/example.doi",
+    link: "https://example.com/fullpaper.pdf",
+  };
+
   return (
     <>
       <Head setProgress={props.setProgress} />
       <Intro setProgress={props.setProgress} />
+      <Research paper={paper} />
       <Careerpath setProgress={props.setProgress} />
-      <Skills setProgress={props.setProgress} />
+      {/* <Skills setProgress={props.setProgress} /> */}
+      <Skills2 setProgress={props.setProgress} />
       <Projects setProgress={props.setProgress} />
       <Social setProgress={props.setProgress} />
       <Footer setProgress={props.setProgress} />
